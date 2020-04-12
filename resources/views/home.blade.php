376fc39d152e83be8c06b29c61b0b265f59eed60
Ritesh Sahu <riteshsahu314@gmail.com>
2020-04-12 12:04:36 +0530

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Vimeo Upload Video</div>
+
+                <div class="card-body">
+                    <button class="btn btn-primary">Upload Video</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
